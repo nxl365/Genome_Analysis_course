592ba6f1ad5e582e408260c66c8132a1c6f02281
dreamy326 <90687628+dreamy326@users.noreply.github.com>
2022-04-10 21:29:16 +0200

    Create 01_DNA_long_assembly_canu.sh

---
+++ code/01_DNA_long_assembly_canu.sh
@@ -0,0 +1,24 @@
+#  /home/nanxi/Genome_anaysis/code/01_DNA_long_assembly_canu.job
+#  batch script:
+
+
+
+#!/bin/bash -l
+#SBATCH -A uppmax2022-2-5
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 4
+#SBATCH -t 20:00:00
+#SBATCH -J Dna_long_assembly_Canu_11
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user nanxing.liu.3019@student.uu.se
+
+module load bioinfo-tools 
+module load canu
+
+canu \
+ -p DNA_pacbio_assembly_11 -d /home/nanxi/Genome_anaysis/analyses/01_DNA_assembly \
+ genomeSize=24m \
+ useGrid=false \
+ -pacbio-raw /home/nanxi/Genome_anaysis/data/raw_data/4_Tean_Teh_2017/pacbio_data/SRR6037732_scaffold_11.fq.gz
+
