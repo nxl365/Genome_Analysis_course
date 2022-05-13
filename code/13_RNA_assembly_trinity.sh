10233de2cd260733b66d8d09a159c5cfc4210755
dreamy326 <90687628+dreamy326@users.noreply.github.com>
2022-05-13 02:17:23 +0200

    Create 13_RNA_assembly_trinity.sh

---
+++ code/13_RNA_assembly_trinity.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -l
+
+#SBATCH -A uppmax2022-2-5
+#SBATCH -p core
+#SBATCH -n 8
+#SBATCH -t 24s:00:00
+#SBATCH -J trinity_11
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user nanxing.liu.3019@student.uu.se
+
+module load bioinfo-tools
+module load samtools 
+module load trinity/2.4.0
+
+# use merged.bam after mapping(star)
+Trinity --genome_guided_bam /proj/genomeanalysis2022/nobackup/work/nanxi/analyses/11_merge_bam/merge/map_sort_merge.bam \
+ --max_memory 100G --genome_guided_max_intron 10000 --CPU 8 \
+--output /proj/genomeanalysis2022/nobackup/work/nanxi/analyses/13_RNA_assembly_trinity/trinity_output
+
+
+# use sorted.bam after mapping(star)
+
+//cd /proj/genomeanalysis2022/nobackup/work/nanxi/analyses/11_merge_bam/sort
+
+// for file in *_sort.bam:
+// do
+//	Trinity --genome_guided_bam $file ----max_memory 100G --genome_guided_max_intron 10000 --CPU 8 \
+//	--output /proj/genomeanalysis2022/nobackup/work/nanxi/analyses/13_RNA_assembly_trinity/${file}_trinity_out
+//done
+
